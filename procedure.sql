7770a4377043008f9c4f43e3db6a18e6ded6c3e7
digital opium <test@ismail.com>
2023-06-22 16:05:41 +0100

    create tables

--- procedure.sql
+++ procedure.sql
@@ -1,3 +1,30 @@
 create database procedure;
 use procedure;
 
+create table member (
+    id INT PRIMARY KEY,
+    name varchar(20),
+    fonction varchar(40)
+);
+
+create table note (
+    member_id INT,
+    costume_id INT,
+    PRIMARY KEY (member_id, costume_id),
+    name varchar(20),
+    note float,
+    foreign key member_id references member on id,
+    foreign key costume_id references costume on id
+);
+
+create table costume (
+    id INT PRIMARY KEY,
+    name varchar(20),
+    style_id INT,
+    foreign key style_id references style on id
+);
+
+create table style (
+    id INT PRIMARY KEY,
+    name varchar(20)
+);
